594982a157ed20a03ba6749dbc9210e306e478a8
asN6a <asN6a>
2024-06-08 13:26:43 -0300

    test

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,9 @@
 // Smartphone Data
 var smartphoneData = [
-  {name: 'iPhone 14 Pro Max', price: "R$6.250,00 <br>6X R$ 1.132,00 <br>12X R$ 595,00", img: "https://cdn.awsli.com.br/2500x2500/761/761012/produto/218353870/editada--5--rcs401z0ta.png"},
-  {name: 'iPhone 14', price: "R$4.550,00 <br>6X R$ 824,00<br>12X R$ 434,00", img: "https://www.atacadogames.com/imagem/smartphones/celular-apple-iphone-14-a2884-ch-256gb-5g-tela-6-1-camera-dupla-branco-sim-fisico/2/184464.jpg?pfdrid_c=true"},
-  {name: 'iPhone 13', price: "R$4.090,00 <br>6X R$ 741,00<br>12X R$ 390,00", img: "https://www.atacadogames.com/imagem/smartphones/celular-apple-iphone-13-pro-256gb-5g-tela-6-1-cameras-de-12mp-12mp-12mp-e-12mp-verde-cpo/2/170624.jpg?pfdrid_c=true"},
-  {name: 'iPhone 11', price: "R$3.199,00 <br>6X R$ 580,00<br>12X R$ 305,00", img: "https://www.atacadogames.com/imagem/smartphones/celular-apple-iphone-11-a2221-lz-64gb-4g-lte-tela-6-1-cameras-de-12mp-12mp-e-12mp-preto-caixa-slim/2/137191.jpg?pfdrid_c=true"}
+  {name: 'Apple iPhone 14 Pro Max 256GB Meia-noite 6,1” 12MP', price: "R$6.250,00", img: "https://cdn.awsli.com.br/2500x2500/761/761012/produto/218353870/editada--5--rcs401z0ta.png"},
+  {name: 'Apple iPhone 14 128GB Meia-noite 6,1” 12MP - iOS 5G', price: "R$4.550,00 no pix", img: "https://www.atacadogames.com/imagem/smartphones/celular-apple-iphone-14-a2884-ch-256gb-5g-tela-6-1-camera-dupla-branco-sim-fisico/2/184464.jpg?pfdrid_c=true"},
+  {name: 'Apple iPhone 13 128GB Estelar Tela 6,1” 12MP', price: "R$4.090,00", img: "https://www.atacadogames.com/imagem/smartphones/celular-apple-iphone-13-pro-256gb-5g-tela-6-1-cameras-de-12mp-12mp-12mp-e-12mp-verde-cpo/2/170624.jpg?pfdrid_c=true"},
+  {name: 'iPhone 11 Apple 128GB Branco 6,1” 12MP iOS', price: "R$3.199,00", img: "https://www.atacadogames.com/imagem/smartphones/celular-apple-iphone-11-a2221-lz-64gb-4g-lte-tela-6-1-cameras-de-12mp-12mp-e-12mp-preto-caixa-slim/2/137191.jpg?pfdrid_c=true"}
 ];
 
 // Smartphone Data System
